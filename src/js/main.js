0eaee52569487262b17b29a60816f48615d64bcb
danielmova <dataxf@gmail.com>
2026-01-28 10:48:33 -0500

    Changing styles for the nav menu and background

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 function switchMode(el) {
   const bodyClass = document.body.classList;
   bodyClass.contains('dark')
-    ? (el.innerHTML = ':Dark Mode:', bodyClass.remove('dark'), localStorage.setItem('theme', 'light'))
-    : (el.innerHTML = ':Light Mode:', bodyClass.add('dark'), localStorage.setItem('theme', 'dark')); 
+    ? (el.innerHTML = 'Dark Mode', bodyClass.remove('dark'), localStorage.setItem('theme', 'light'))
+    : (el.innerHTML = 'Light Mode', bodyClass.add('dark'), localStorage.setItem('theme', 'dark')); 
 }
 
 //
@@ -12,12 +12,12 @@ function restoreTheme() {
   
   if (savedTheme === 'dark') {
     document.body.classList.add('dark');
-    if (themeBtn) themeBtn.innerHTML = ':Light Mode:';
+    if (themeBtn) themeBtn.innerHTML = 'Light Mode';
   } 
   
   else if (savedTheme === 'light') {
     document.body.classList.remove('dark');
-    if (themeBtn) themeBtn.innerHTML = ':Dark Mode:';
+    if (themeBtn) themeBtn.innerHTML = 'Dark Mode';
   }
 }
 
